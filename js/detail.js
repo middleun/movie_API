046ee4a1c73dc0c91f0e286594630d39b105477b
middleun <eunallaco@gmail.com>
2021-03-04 17:05:07 +0900

    210304_detail page 내 cast 데이터 반복문 설정 ( 데이터 없을 때???? 문제 해결 예정 ) / detail.js 따로 생성

---
+++ js/detail.js
@@ -0,0 +1,92 @@
+$(function(){
+    // 주소 읽는 함수 cf.search.js
+
+    let getDetail=[];
+    let detailID = document.location.href.split("=")[1];
+    // console.log(detailID);
+
+    
+
+    $.ajax({
+        url:'https://yts.mx/api/v2/movie_details.json?movie_id='+detailID+'&with_images=true&with_cast=true',
+        success: function(detailData){
+            // console.log(detailData.data.movie.cast[0].name);
+
+            let castName = [];
+            for(let i=0; i < detailData.data.movie.cast.length; i++){
+                let castLi = `<b>${detailData.data.movie.cast[i].name +'</br>'}</b>`;
+                castName += castLi;
+
+                if(!detailData.data.movie.cast){
+                    castName = "No Actor";
+                }
+            }
+
+            console.log(castName);
+
+            //for(let i=0; i < detailData.data.movie.cast.length; i++){
+                // if(!detailData.data.movie.cast){
+                //     $(".detailCast").css({"display":"none"});
+                // }
+
+                // if(detailData.data.movie.cast.length == 0){
+                //     detailData.data.movie.cast[0].name = 'No Data';
+                // }
+
+                let detailHTML = `<div class="detailUp clear">
+                                    <div class="detailImg">
+                                        <img src="${detailData.data.movie.medium_cover_image}" alt="">
+                                    </div>
+                                    <div class="detailInfo">
+                                        <h3>${detailData.data.movie.title}</h3>
+                                        <ul>
+                                            
+                                            <li class="detailYear">${detailData.data.movie.year}</li>
+                                            <li class="detailCast"></li>
+                                            <li class="detailGenre">${detailData.data.movie.genres}</li>
+                                            <li class="detailRating"><i class="fas fa-star">${detailData.data.movie.rating}</i></li>
+                                        </ul>
+                                    </div>
+                                    
+                                    <div class="detailTxt">
+                                        <h3>detail</h3>
+                                        <div class="txtBox">
+                                            <span class="detailDes">${detailData.data.movie.description_intro}</span>
+                                        </div>
+
+                                    </div>
+                                </div>   
+
+                                <div class="detailDown">
+                                    <div class="detailThumb">
+                                        <ul>
+                                            <li>
+                                                <img src="${detailData.data.movie.medium_screenshot_image1}" alt="">  
+                                            </li>
+                                            <li>
+                                                <img src="${detailData.data.movie.medium_screenshot_image2}" alt="">  
+                                            </li>
+                                            <li>
+                                                <img src="${detailData.data.movie.medium_screenshot_image3}" alt="">  
+                                            </li>                                               
+                                        </ul>
+
+                                    </div>
+                                </div>
+                                
+                                <!-- end of detail section -->`;
+                getDetail = detailHTML; 
+                // console.log(detailData.data.movie.cast[0].name);
+                $(".detailSec").append(detailHTML);
+                $(".detailCast").append(castName);
+            //}
+
+           
+        }
+       
+
+    });
+
+
+
+});
